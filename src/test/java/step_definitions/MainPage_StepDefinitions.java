828c59e0634faf8964b05fed0fe58bbf308b34a5
bulutserbest <43551972+bulutserbest@users.noreply.github.com>
2018-10-09 14:09:46 +0300

    Paris Otel First Search Scenario & Step Definitions Added;

--- src/test/java/step_definitions/MainPage_StepDefinitions.java
+++ src/test/java/step_definitions/MainPage_StepDefinitions.java
@@ -65,4 +65,118 @@ public class MainPage_StepDefinitions {
     public void i_should_see_Agoda_homepage() throws Throwable {
         Assert.assertEquals(true, mainPage.validateLoginValidationClass());
     }
+
+    @Given("^I am on the Agoda homepage$")
+    public void ı_am_on_the_Agoda_homepage() throws Throwable {
+
+    }
+
+    @When("^I will click 'Giris Yapin' link$")
+    public void ı_will_click_Giris_Yapin_link() throws Throwable {
+
+    }
+
+    @Then("^I should see Login pop -up$")
+    public void ı_should_see_Login_pop_up() throws Throwable {
+
+    }
+
+    @Then("^I will fill 'E-posta'$")
+    public void ı_will_fill_E_posta() throws Throwable {
+
+    }
+
+    @Then("^I will fill 'Sifre'$")
+    public void ı_will_fill_Sifre() throws Throwable {
+
+    }
+
+    @Then("^I will click 'Giris Yapin' button$")
+    public void ı_will_click_Giris_Yapin_button() throws Throwable {
+
+    }
+
+    @Then("^I should see Agoda homepage$")
+    public void ı_should_see_Agoda_homepage() throws Throwable {
+
+    }
+
+    @Given("^I must be logged in Agoda website$")
+    public void ı_must_be_logged_in_Agoda_website() throws Throwable {
+
+    }
+
+    @When("^I will click 'Fiyat Goruntuleme'$")
+    public void ı_will_click_Fiyat_Goruntuleme() throws Throwable {
+
+    }
+
+    @Then("^I should see 'Fiyat Goruntuleme' pop-up$")
+    public void ı_should_see_Fiyat_Goruntuleme_pop_up() throws Throwable {
+
+    }
+
+    @Then("^I will click 'Avro'$")
+    public void ı_will_click_Avro() throws Throwable {
+
+    }
+
+    @Then("^I should see Agoda$")
+    public void ı_should_see_Agoda() throws Throwable {
+
+    }
+
+    @Then("^I will fill first search-box 'Paris'$")
+    public void ı_will_fill_first_search_box_Paris() throws Throwable {
+
+    }
+
+    @Then("^I will fill second search-box '(\\d+)\\.(\\d+)\\.(\\d+)'$")
+    public void ı_will_fill_second_search_box(int arg1, int arg2, int arg3) throws Throwable {
+
+    }
+
+    @Then("^I will fill third search-box '(\\d+)\\.(\\d+)\\.(\\d+)'$")
+    public void ı_will_fill_third_search_box(int arg1, int arg2, int arg3) throws Throwable {
+
+    }
+
+    @Then("^I should see pop-up menu$")
+    public void ı_should_see_pop_up_menu() throws Throwable {
+
+    }
+
+    @Then("^I will choose (\\d+) adults$")
+    public void ı_will_choose_adults(int arg1) throws Throwable {
+
+    }
+
+    @Then("^I will choose (\\d+) child$")
+    public void ı_will_choose_child(int arg1) throws Throwable {
+
+    }
+
+    @Then("^I will choose child age (\\d+)$")
+    public void ı_will_choose_child_age(int arg1) throws Throwable {
+
+    }
+
+    @Then("^I will click 'Aile odasi tercih ediyorum'$")
+    public void ı_will_click_Aile_odasi_tercih_ediyorum() throws Throwable {
+
+    }
+
+    @Then("^I will click 'Ara' button$")
+    public void ı_will_click_Ara_button() throws Throwable {
+
+    }
+
+    @Then("^I should see the products are listed$")
+    public void ı_should_see_the_products_are_listed() throws Throwable {
+
+    }
+
+
+
+
 }
--- src/test/resources/features/Agoda.feature
+++ src/test/resources/features/Agoda.feature
@@ -8,3 +8,20 @@ Feature: Automatron Team Agoda Test Automation Project Feature
     Then I will fill 'Sifre'
     Then I will click 'Giris Yapin' button
     Then I should see Agoda homepage
+
+  Scenario: Paris Otel First Search
+    Given I must be logged in Agoda website
+    When I will click 'Fiyat Goruntuleme'
+    Then I should see 'Fiyat Goruntuleme' pop-up
+    And I will click 'Avro'
+    Then I should see Agoda
+    And I will fill first search-box 'Paris'
+    And I will fill second search-box '20.04.2019'
+    And I will fill third search-box '23.04.2019'
+    Then I should see pop-up menu
+    And I will choose 2 adults
+    And I will choose 1 child
+    And I will choose child age 2
+    And I will click 'Aile odasi tercih ediyorum'
+    And I will click 'Ara' button
+    Then I should see the products are listed
